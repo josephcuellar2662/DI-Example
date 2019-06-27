aea2930a94f668b2526c3839f8fafc3df6d1b13a
josephcuellar <josephcuellar2662@gmail.com>
2019-06-26 19:24:32 -0600

    Added controller and instaniated with application context

--- src/main/java/com/springframework/diexample/DiExampleApplication.java
+++ src/main/java/com/springframework/diexample/DiExampleApplication.java
@@ -1,13 +1,20 @@
 package com.springframework.diexample;
 
+import com.springframework.diexample.controllers.MyController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class DiExampleApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DiExampleApplication.class, args);
+
+        ApplicationContext ctc = SpringApplication.run(DiExampleApplication.class, args);
+
+        MyController controller = (MyController) ctc.getBean("myController");
+
+        controller.hello();
     }
 
 }
